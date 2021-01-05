1796e963291682cb82af04a2c7bc30cc591c9eb6
evaperon <evelescout@gmail.com>
2021-01-05 21:48:24 +0200

    generate all dot files with weighted and labeled edges

---
+++ generateAllDots.py
@@ -0,0 +1,12 @@
+import glob
+
+with open('generatdots.sh', 'w') as script:
+    script.write("#! /bin/bash\n")
+
+    all_xlsxs = []
+    for xlsx in glob.iglob("xlsxs/*"):
+        no_prefix = xlsx.replace('xlsxs/','')
+        all_xlsxs.append(no_prefix)
+    
+    for xlsx in all_xlsxs:
+        script.write("python generateDot.py {file}\n".format(file=xlsx))
--- generateDot.py
+++ generateDot.py
@@ -8,6 +8,11 @@ def main():
     args = sys.argv
     filename =  args[1]
     fname = "xlsxs/"+filename
+    split_name = filename.split('-')
+    last_part = split_name[3]
+    last_split = last_part.split('.')
+    label = int(last_split[0])
+    print("File: {f} , label = {l}".format(f=filename, l=label))
 
     # graph_data is an array of edges (dictionaries)
     graph_data = []
@@ -19,6 +24,7 @@ def main():
         temp_dict['source']=row[0]
         temp_dict['dest']=row[1]
         temp_dict['weight']=row[2]
+        temp_dict['label']=label
         graph_data.append(temp_dict)
 
     # print("Source = {s}, Destination = {d}. Weight = {w}".format(s=graph_data[0]['source'], d=graph_data[0]['dest'], w=graph_data[0]['weight']))
@@ -26,7 +32,7 @@ def main():
     # create a directed graph
     graph = pgv.AGraph(directed=True)
     for edge in graph_data:
-        graph.add_edge(str(edge['source']), str(edge['dest']), weight=edge['weight'])
+        graph.add_edge(str(edge['source']), str(edge['dest']), weight=edge['weight'], label=edge['label'])
 
     graph.write("dots/"+filename+'.dot')
     # graph.layout(prog='dot')
